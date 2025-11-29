ed36920587d04260e7839070afb8f2fa5dd86c5b
Rahul <rahu8299@gmail.com>
2025-11-29 19:39:47 +0530

    adding placeholder apis

--- Backend/main.py
+++ Backend/main.py
@@ -21,3 +21,23 @@ models.Base.metadata.create_all(bind=database.engine)
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/api/appointments")
+async def get_appointments():
+    return {"message": "Get all appointments"}
+
+
+@app.get("/api/appointments/available")
+async def get_available_slots():
+    return {"message": "Get available time slots"}
+
+
+@app.post("/api/appointments")
+async def create_appointment(appointment: schema.AppointmentCreate):
+    return {"message": "Create appointment"}
+
+
+@app.delete("/api/appointments/{id}")
+async def cancel_appointment(id: int):
+    return {"message": f"Cancel appointment with id {id}"}
